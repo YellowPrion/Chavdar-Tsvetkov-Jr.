d668cd4234f74f4f1fb8d1823d54d63628799521
Chavdar Tsvetkov <89222199+YellowPrion@users.noreply.github.com>
2021-10-11 17:41:24 +0300

    Delete Exercise 2.py

--- Exercise 2.py
+++
@@ -1,7 +0,0 @@
-string  = tuple(map(int, (input().split())))
-n = string[0]
-m = string[1]
-numbers = {input() for b in range(n)}
-second_numbers = {input() for c in range(m)}
-both = numbers & second_numbers
-print('\n'.join(both))
